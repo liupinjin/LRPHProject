9180022551cd04eb0e661fb5cfa61840d4efb662
han.chen <han.chen@husor.com>
2019-05-28 14:23:11 +0800

    依赖httplib

---
+++ pn_sdk/src/main/java/com/punuo/sys/app/httplib/IHttpConfig.java
@@ -0,0 +1,16 @@
+package com.punuo.sys.app.httplib;
+
+/**
+ * Created by han.chen.
+ * Date on 2019/4/23.
+ **/
+public interface IHttpConfig {
+
+    String getHost();
+
+    int getPort();
+
+    boolean isUseHttps();
+
+    String getUserAgent();
+}
