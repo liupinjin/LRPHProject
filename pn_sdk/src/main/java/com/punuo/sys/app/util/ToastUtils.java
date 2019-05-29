1736a9b89223d6754a1e475aab23bb1e7a0e13fc
han.chen <han.chen@husor.com>
2019-05-29 14:14:14 +0800

    ToastUtils移到sdk

--- pn_sdk/src/main/java/com/punuo/sys/app/util/ToastUtils.java
+++ pn_sdk/src/main/java/com/punuo/sys/app/util/ToastUtils.java
@@ -8,15 +8,6 @@ import com.punuo.sys.app.PnApplication;
 
 public class ToastUtils {
 	private static Toast mToast;
-
-	/**
-	 * 为解决 http://mobile.umeng.com/apps/161e9078c5b0426594dc3a35/error_types/show?error_type_id=53a3cd4956240b5c8709e161_7212916063408484620_4.7.0<p/>
-	 * 增加try...catch...
-	 * 单独setText是为了解决新版MIUI会在text前边加上appName问题
-	 * 显示Toast
-	 *
-	 * @param text
-	 */
 	public static void showToast(CharSequence text) {
 		if (TextUtils.isEmpty(text)) return;
 
@@ -35,13 +26,6 @@ public class ToastUtils {
 		}
 	}
 
-	/**
-	 * 为解决 http://mobile.umeng.com/apps/161e9078c5b0426594dc3a35/error_types/show?error_type_id=53a3cd4956240b5c8709e161_7212916063408484620_4.7.0<p/>
-	 * 增加try...catch...
-	 * 显示Toast
-	 *
-	 * @param text
-	 */
 	public static void showToastShort(CharSequence text) {
 		if (TextUtils.isEmpty(text)) return;
 
