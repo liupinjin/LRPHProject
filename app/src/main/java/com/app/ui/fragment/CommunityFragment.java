d7b1ae5db2ae641f39f7253c5d28aaf512af68fa
Maojianhui <1131883677@qq.com>
2019-07-30 13:38:44 +0800

    版本无法更新bug修复

--- app/src/main/java/com/app/ui/fragment/CommunityFragment.java
+++ app/src/main/java/com/app/ui/fragment/CommunityFragment.java
@@ -3,6 +3,7 @@ package com.app.ui.fragment;
 
 import android.support.v4.app.Fragment;
 
+import com.app.sip.SipInfo;
 import com.punuo.sys.app.fragment.WebViewFragment;
 
 /**
@@ -12,7 +13,7 @@ public class CommunityFragment extends WebViewFragment {
 
     @Override
     public String getUrl() {
-        return "http://pet.qinqingonline.com:8889";
+        return "http://pet.qinqingonline.com:8889"+ SipInfo.userId;
     }
 }
 
